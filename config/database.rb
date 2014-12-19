0001451f181d513fe1cc003c728b8f7ec33f7e0a
Dylan Krause <dykrause@gmail.com>
2014-12-19 00:16:11 -0500

    Establish db name -allow for heroku to write its own DB name in the ENV

--- config/database.rb
+++ config/database.rb
@@ -9,3 +9,9 @@ Dir[APP_ROOT.join('app', 'models', '*.rb')].each do |model_file|
   filename = File.basename(model_file).gsub('.rb', '')
   autoload ActiveSupport::Inflector.camelize(filename), model_file
 end
+
+# Heroku controls what database we connect to by setting the DATABASE_URL environment variable
+# We need to respect that if we want our Sinatra apps to run on Heroku without modification
+db = URI.parse(ENV['DATABASE_URL'] || "postgres://localhost/#{APP_NAME}_#{Sinatra::Application.environment}")
+
+DB_NAME = db.path[1..-1]
