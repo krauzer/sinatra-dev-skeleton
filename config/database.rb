36877b22dbd3dea1379de47918937bc4bacc2ab4
Dylan Krause <dykrause@gmail.com>
2014-12-19 00:14:15 -0500

    Setup loading of models for db purposes

--- config/database.rb
+++ config/database.rb
@@ -2,3 +2,10 @@
 if Sinatra::Application.development?
   ActiveRecord::Base.logger = Logger.new(STDOUT)
 end
+
+#Load from the APP's app/models folder all models
+#Prevent from inheritence based exceptions on class definitions i.e. if a child is required before its parent
+Dir[APP_ROOT.join('app', 'models', '*.rb')].each do |model_file|
+  filename = File.basename(model_file).gsub('.rb', '')
+  autoload ActiveSupport::Inflector.camelize(filename), model_file
+end
