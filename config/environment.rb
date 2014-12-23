f7229415c39402d6b2a2918cbea5e010396151fd
Dylan Krause <dykrause@gmail.com>
2014-12-23 17:40:10 -0500

    Set public_folder settings attribute to temp folder during development

--- config/environment.rb
+++ config/environment.rb
@@ -38,6 +38,10 @@ configure do
   set :views, File.join(Sinatra::Application.root, "app", "views")
 end
 
+configure :development do
+  set :public_folder, File.join(Sinatra::Application.root, "tmp")
+end
+
 # Set up the controllers and helpers
 Dir[APP_ROOT.join('app', 'controllers', '*.rb')].each { |file| require file }
 Dir[APP_ROOT.join('app', 'helpers', '*.rb')].each { |file| require file }
