de1086e2a01fd40ba84649fe98a53f0e7f673dec
Dylan Krause <dykrause@gmail.com>
2015-01-01 14:44:12 -0500

    Change controllers folder to routes folder and change its reference in the environment file

--- config/environment.rb
+++ config/environment.rb
@@ -46,8 +46,8 @@ configure :production do
   set :views, File.join(Sinatra::Application.root, "dist", "templates")
 end
 
-# Set up the controllers and helpers
-Dir[APP_ROOT.join('app', 'controllers', '*.rb')].each { |file| require file }
+# Set up the controllers via routes and helpers
+Dir[APP_ROOT.join('app', 'routes', '*.rb')].each { |file| require file }
 Dir[APP_ROOT.join('app', 'helpers', '*.rb')].each { |file| require file }
 
 # Set up the database and models
